f2fe4c003084b76f9f7ef00aa845694dca754a68
Christian Haas <christian.haas@sevensuns.at>
2022-02-12 09:16:20 +0100

    Add initial Spring setup

---
+++ src/main/java/com/example/e2edemo/E2eDemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.e2edemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2eDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(E2eDemoApplication.class, args);
+    }
+}
